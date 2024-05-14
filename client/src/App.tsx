78a273e999674109235f427e41f649fbb52c2dee
Igor Shilov <borkts@gmail.com>
2024-05-14 19:16:08 +0500

    final

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,16 @@
+import { useEffect } from "react";
 import "./App.css";
 import AllMuseums from "./pages/AllMuseums/AllMuseums";
+import { fetchAuth } from "./redux/thunkActions";
+import { useAppDispatch } from "./redux/hooks";
 
 function App():JSX.Element  {
+const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(fetchAuth());
+  }, []);
+
   return (
   <>
   <AllMuseums />
