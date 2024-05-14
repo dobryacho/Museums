118e3432dcacb5c199e6840ac1911a93a3de39b1
Shamil <shamil.erkenov2000@mail.ru>
2024-05-14 14:35:18 +0300

    added database

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,29 +1,29 @@
-const express = require("express");
-const bcrypt = require("bcrypt");
-const { User } = require("../db/models");
+const express = require('express');
+const bcrypt = require('bcrypt');
+const { User } = require('../db/models');
 
 const router = express.Router();
 
-router.post("/login", async (req, res) => {
+router.post('/login', async (req, res) => {
   const { login, password } = req.body;
   const user = await User.findOne({ where: { login } });
   if (!user) {
-    return res.json({ err: "Такого пользователя нет, зарегистрируйтесь" });
+    return res.json({ err: 'Такого пользователя нет, зарегистрируйтесь' });
   }
   const checkPass = await bcrypt.compare(password, user.password);
   if (checkPass) {
     req.session.login = user.login;
     res.json(user);
   } else {
-    res.json({ err: "Пароль неверный" });
+    res.json({ err: 'Пароль неверный' });
   }
 });
 
-router.post("/", async (req, res) => {
+router.post('/', async (req, res) => {
   const { login, password } = req.body;
   const user = await User.findOne({ where: { login } });
   if (user) {
-    return res.json({ err: "Такой пользователь уже есть" });
+    return res.json({ err: 'Такой пользователь уже есть' });
   }
   const hash = await bcrypt.hash(password, 10);
   const newUser = await User.create({ login, password: hash });
@@ -33,14 +33,14 @@ router.post("/", async (req, res) => {
   });
 });
 
-router.get("/logout", (req, res) => {
+router.get('/logout', (req, res) => {
   req.session.destroy(() => {
-    res.clearCookie("cookieName");
+    res.clearCookie('cookieName');
     res.end();
   });
 });
 
-router.get("/auth", async (req, res) => {
+router.get('/auth', async (req, res) => {
   console.log(req.session);
   if (req.session?.login) {
     const user = await User.findOne({ where: { login: req.session.login } });
