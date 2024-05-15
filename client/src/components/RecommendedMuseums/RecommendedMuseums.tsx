7f47967f85dfcc5db531264a7eed9594c1edf446
Shamil <shamil.erkenov2000@mail.ru>
2024-05-15 09:40:48 +0300

    prev merge with dev

--- client/src/components/RecommendedMuseums/RecommendedMuseums.tsx
+++ client/src/components/RecommendedMuseums/RecommendedMuseums.tsx
@@ -8,15 +8,12 @@ export default function Navbar() {
       const response = await fetch('http://localhost:3000/api/museums');
       const data = await response.json();
 
-      // Функция для генерации случайного числа в заданном диапазоне
       function getRandomInt(min: number, max: number) {
         return Math.floor(Math.random() * (max - min + 1)) + min;
       }
 
-      // Новый массив, куда будут добавлены случайные элементы
       const recommendedMuseums = [];
 
-      // Цикл для выбора и добавления 4 случайных элементов в новый массив
       while (recommendedMuseums.length < 4) {
         const randomIndex = getRandomInt(0, data.length - 1);
         if (!recommendedMuseums.includes(data[randomIndex])) {
