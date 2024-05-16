2cfb120d7a284d020d28945ef4f4998b7d32d214
Igor Shilov <borkts@gmail.com>
2024-05-16 15:54:46 +0500

    map done

---
+++ client/src/components/MapMuseums/MapMuseuns.tsx
@@ -0,0 +1,63 @@
+import { Map, Placemark, YMaps } from '@pbe/react-yandex-maps';
+import { useAppSelector } from '../../redux/hooks';
+import { useEffect, useState } from 'react';
+
+const piter = { center: [59.938573235606746, 30.323395361055585], zoom: 11 };
+const moscow = { center: [55.75, 37.57], zoom: 11 };
+
+type TopLevel = {
+  id:          number;
+  name:        string;
+  description: string;
+  location:    string;
+  city:        string;
+  photo:       string;
+  workedTime:  string;
+  holidays:    string;
+  theme:       string;
+  coordinates: string,
+  createdAt:   Date;
+  updatedAt:   Date;
+}
+
+
+function MapMuseuns({ allMuseums }: {allMuseums: TopLevel[]}) {
+  const user = useAppSelector((store) => store.userSlice.user);
+  const [load, setLoad] = useState(false);
+
+  useEffect(() => {
+    if (allMuseums?.length) {
+      setLoad(true);
+    }
+  }, [allMuseums]);
+
+  return (
+    <>
+      {load ? (
+        <YMaps>
+          <Map
+            defaultState={user?.city === 'petersburg' ? piter : moscow}
+            width={'90vw'}
+            height={700}
+          >
+            {allMuseums.map((el) => (
+              <Placemark
+                key={el.id}
+                modules={['geoObject.addon.balloon']}
+                defaultGeometry={el.coordinates.match(/\d*\.\d*/gi) || moscow.center}
+                properties={{
+                  balloonContentBody: `<img src="${el.photo}" style="width: 100px;height: auto;" align="left" vspace="5" hspace="5"></img> <b>${el.name}</b><p>${el.description}</p>
+            <p>время работы: ${el.workedTime}</p>`,
+                }}
+              />
+            ))}
+          </Map>
+        </YMaps>
+      ) : (
+        <div>Загрузка</div>
+      )}
+    </>
+  );
+}
+
+export default MapMuseuns;
