1dbfebb7de07f2483bd3663e6aa44b1234505939
Shamil <shamil.erkenov2000@mail.ru>
2024-05-16 13:37:31 +0300

    Надобно

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,11 +4,14 @@ import App from './App.tsx';
 import './index.css';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
+import { ChakraProvider } from '@chakra-ui/react'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
+    <ChakraProvider>
     <BrowserRouter>
       <App />
     </BrowserRouter>
+    </ChakraProvider>
   </Provider>
 )
