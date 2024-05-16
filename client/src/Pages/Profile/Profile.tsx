8105a3e7c49c35d151e7e69c9442c0daa093184f
Shamil <shamil.erkenov2000@mail.ru>
2024-05-16 18:13:46 +0300

    favnews and favmuseums

--- client/src/Pages/Profile/Profile.tsx
+++ client/src/Pages/Profile/Profile.tsx
@@ -1,9 +1,11 @@
 import FavoriteNews from '../../components/FavoriteNews/FavoriteNews';
+import FavoritesMuseums from '../../components/FavoritesMuseums/FavoritesMuseums';
 
 export default function Profile() {
   return (
     <div>
       <FavoriteNews />
+      <FavoritesMuseums />
     </div>
   );
 }
--- client/src/components/FavoritesMuseums/FavoritesMuseums.tsx
+++ client/src/components/FavoritesMuseums/FavoritesMuseums.tsx
@@ -1,7 +1,7 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
-import { useAppSelector } from "../../redux/hooks";
-import FavoriteMuseum from "../FavoriteMuseum/FavoriteMuseum";
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { useAppSelector } from '../../redux/hooks';
+import FavoriteMuseum from '../FavoriteMuseum/FavoriteMuseum';
 
 type MuseumType = {
   id: number;
@@ -23,28 +23,30 @@ type Museums = Array<MuseumType>;
 export default function FavoritesMuseums() {
   const user = useAppSelector((store) => store.userSlice.user);
 
-  const [favorites, setFavorites] = useState<Museums>([])
+  const [favorites, setFavorites] = useState<Museums>([]);
 
   useEffect(() => {
     if (user.id) {
-      axios.get(`http://localhost:3000/api/user/favorites/${user.id}`).then((res) => {
-        const [userData] = res.data
-        setFavorites(userData?.favoriteMuseums);
-        console.log(userData?.favoriteMuseums);
-      })
+      axios
+        .get(`http://localhost:3000/api/user/favorites/${user.id}`)
+        .then((res) => {
+          const [userData] = res.data;
+          setFavorites(userData?.favoriteMuseums);
+          console.log(userData?.favoriteMuseums);
+        });
     }
   }, [user.id]);
-  
 
   return (
     <div>
-    {favorites.length ? (
-      favorites.map((museum) => (
-        <FavoriteMuseum key={museum.id} museum={museum} />
-      ))
-    ) : (
-      <h3>Нет избранных музеев</h3>
-    )}
-  </div>
-  )
+      <h2>Ваши любимые музеи</h2>
+      {favorites.length ? (
+        favorites.map((museum) => (
+          <FavoriteMuseum key={museum.id} museum={museum} />
+        ))
+      ) : (
+        <h3>Нет избранных музеев</h3>
+      )}
+    </div>
+  );
 }
