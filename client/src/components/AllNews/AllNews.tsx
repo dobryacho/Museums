8de14d0940fa75d5a1520ff3e5139ffd54ae52e1
Shamil <shamil.erkenov2000@mail.ru>
2024-05-22 12:12:07 +0300

    fix addmuseum

--- client/src/components/AllNews/AllNews.tsx
+++ client/src/components/AllNews/AllNews.tsx
@@ -8,21 +8,33 @@ export default function AllNews() {
   const { t, i18n } = useTranslation();
   const [news, setNews] = useState([]);
   const user = useAppSelector((store) => store.userSlice.user);
-  const toast = useToast()
+  const toast = useToast();
 
   useEffect(() => {
     const getAllNews = async () => {
       // Получение всех новостей
-      const response = await fetch(`http://localhost:3000/api/news?lang=${i18n.language}`);
+      const response = await fetch(
+        `http://localhost:3000/api/news?lang=${i18n.language}`,
+      );
       const data = await response.json();
 
       let newsToShow; // Переменная для хранения отфильтрованных музеев
 
       // Фильтрация новостей по городу
       if (userCity === 'moscow') {
-        newsToShow = data.filter((el) => el.museumCity === 'Москва' || el.museumCity === 'Moscow' || el.museumCity === 'Moskau');
+        newsToShow = data.filter(
+          (el) =>
+            el.museumCity === 'Москва' ||
+            el.museumCity === 'Moscow' ||
+            el.museumCity === 'Moskau',
+        );
       } else {
-        newsToShow = data.filter((el) => el.museumCity === 'Санкт-Петербург' || el.museumCity === 'Saint Petersburg' || el.museumCity === 'Sankt Petersburg');
+        newsToShow = data.filter(
+          (el) =>
+            el.museumCity === 'Санкт-Петербург' ||
+            el.museumCity === 'Saint Petersburg' ||
+            el.museumCity === 'Sankt Petersburg',
+        );
       }
 
       // Сортировка по дате
@@ -37,17 +49,21 @@ export default function AllNews() {
   }, [user, i18n.language]);
 
   const handleDelete = (e: any) => {
-      axios.delete(`http://localhost:3000/api/news/${e.target.parentNode.id}`).then(()=>{
-        setNews((data)=> ([...data.filter((el)=> el.id !== Number(e.target.parentNode.id))]))
+    axios
+      .delete(`http://localhost:3000/api/news/${e.target.parentNode.id}`)
+      .then(() => {
+        setNews((data) => [
+          ...data.filter((el) => el.id !== Number(e.target.parentNode.id)),
+        ]);
         toast({
           title: `новость удалена`,
           status: 'success',
           isClosable: true,
           duration: 1000,
           position: 'bottom-right',
-        })        
-      })
-  }
+        });
+      });
+  };
 
   return (
     <div>
@@ -62,13 +78,21 @@ export default function AllNews() {
           <div key={el.id} id={`${el.id}`}>
             <h4>{el.title}</h4>
             <img src={el.photo} alt="Тут должно быть фото музея" />
-            {user.email === "admin_museums@mail.ru" && (
-                  <Button onClick={handleDelete} id='delete'  m={2}>Удалить новость</Button>
-              )}
+            {user.email === 'admin_museums@mail.ru' && (
+              <Button onClick={handleDelete} id="delete" m={2}>
+                Удалить новость
+              </Button>
+            )}
             <p>{el.text}</p>
-            <p>{t('eventPlace')} {el.museumName}.</p>
-            <p>{t('eventDate')} {formattedDate}.</p>
-            <p>{t('address')} {el.museumLocation}.</p>
+            <p>
+              {t('eventPlace')} {el.museumName}.
+            </p>
+            <p>
+              {t('eventDate')} {formattedDate}.
+            </p>
+            <p>
+              {t('address')} {el.museumLocation}.
+            </p>
           </div>
         );
       })}
