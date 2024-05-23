50f2cf72e11c9950759a69d852392bdea679dd40
Shamil <shamil.erkenov2000@mail.ru>
2024-05-23 11:46:02 +0300

    currentMuseum styles done

--- client/src/components/Checkbox/Checkbox.tsx
+++ client/src/components/Checkbox/Checkbox.tsx
@@ -2,9 +2,12 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 import { useAppDispatch, useAppSelector } from '../../redux/hooks';
 import { useTranslation } from 'react-i18next';
+import styles from './Checkbox.module.css';
 
 import {
-  fetchVisited, fetchAddVisited,fetchRemoveVisited,
+  fetchVisited,
+  fetchAddVisited,
+  fetchRemoveVisited,
 } from '../../redux/thunkActionsCurrentMuseum';
 
 import type { RouteParams } from '../../Pages/CurrentMuseum/currMusTypes';
@@ -17,30 +20,34 @@ export default function Checkbox() {
 
   const user = useAppSelector((store) => store.userSlice.user);
   const visitedMuseums = useAppSelector((store) => store.visitedSlice.visited);
-  const isVisited = visitedMuseums.some((vis) => vis.museumId === parseInt(id, 10) && vis.userId === user.id);
+  const isVisited = visitedMuseums.some(
+    (vis) => vis.museumId === parseInt(id, 10) && vis.userId === user.id,
+  );
 
   const handleVisitedClick = () => {
     if (id) {
       const visitedMuseum = visitedMuseums.find(
-        (vis) => vis.museumId === parseInt(id, 10) && vis.userId === user.id
+        (vis) => vis.museumId === parseInt(id, 10) && vis.userId === user.id,
       );
-  
+
       if (visitedMuseum) {
         dispatch(fetchRemoveVisited(visitedMuseum.id));
       } else {
-        dispatch(fetchAddVisited({ userId: user.id, museumId: parseInt(id, 10) }));
+        dispatch(
+          fetchAddVisited({ userId: user.id, museumId: parseInt(id, 10) }),
+        );
       }
     }
   };
 
   return (
-    <div>
-            <input
-              type="checkbox"
-              checked={isVisited}
-              onChange={handleVisitedClick}
-            />
-            <label>{t('visited')}</label>
-          </div>
-  )
+    <div className={styles.wrapper}>
+      <input className={styles.input}
+        type="checkbox"
+        checked={isVisited}
+        onChange={handleVisitedClick}
+      />
+      <label>{t('visited')}</label>
+    </div>
+  );
 }
